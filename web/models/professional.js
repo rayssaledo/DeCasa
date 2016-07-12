9021d48051bd15166ff0bcc576fad12947f2b678
Andreza Raquel <andreza.queiroz@ccc.ufcg.edu.br>
2016-07-12 13:21:43 -0300

    Modificações no cadastro .

--- web/models/professional.js
+++ web/models/professional.js
@@ -13,43 +13,40 @@ module.exports = function(mongodb, app, professionalCollection) {
 		var password = req.body.password;	
 		var passwordRepeat = req.body.passwordRepeat;
 		var personalName = req.body.personalName;
-		
-		var inconsistency = checkInconsistency(businessName, personalName, phone, cpf, email, address, username, password, passwordRepeat);
-
-		if (inconsistency) {
-			res.send(inconsistency);
-		} else {
-			professionalCollection.find({
-				username: username,
-				password: password
-				
-			}).toArray(function(err, array){
-				if (array.length > 0) {
-					res.json({ "ok" : 0, "msg" : "Profissional já cadastrado!" });
-				} else {
+		var services = req.body.services;
 					
-					professionalCollection.insert({
-						username: username,
-						password: password,
-						cpf: cpf,
-						businessName: businessName,
-						email: email,
-						address: address,
-						phone: phone,
-						socialNetwork: socialNetwork,
-						pictury: pictury,
-						site: site
-						
-					}, function (err, doc) {
-						if(err){
-							res.json({ "ok" : 0, "msg" : err });
-						} else {
-							res.json({"ok": 1, "msg": "Cadastro realizado com sucesso!"});	
-						}
-					});
-				}
-			});
-		}
+		professionalCollection.find({
+			username: username,
+			password: password
+			
+		}).toArray(function(err, array){
+			if (array.length > 0) {
+				res.json({ "ok" : 0, "msg" : "Profissional já cadastrado!" });
+			} else {
+				
+				professionalCollection.insert({
+					username: username,
+					password: password,
+					cpf: cpf,
+					businessName: businessName,
+					email: email,
+					address: address,
+					phone: phone,
+					socialNetwork: socialNetwork,
+					pictury: pictury,
+					site: site, 
+					services: services
+					
+				}, function (err, doc) {
+					if(err){
+						res.json({ "ok" : 0, "msg" : err });
+					} else {
+						res.json({"ok": 1, "msg": "Cadastro realizado com sucesso!"});	
+					}
+				});
+			}
+		});
+		
 	});
 	
 	app.post("/check-username", function(req, res) {
@@ -126,48 +123,5 @@ module.exports = function(mongodb, app, professionalCollection) {
 		}
 	}
 	
-	function checkInconsistency(businessName, personalName, phone, cpf, email, address, username, password, passwordRepeat) {
-		if (!businessName) {
-			return '{ "ok" : 0, "msg" : "Nome do negócio deve ser informado!" }';
-		}
-		
-		if (!personalName) {
-			return '{ "ok" : 0, "msg" : "Seu nome deve ser inserido!" }';
-		}
-		
-		if (!phone) {
-			return '{ "ok" : 0, "msg" : "Telefone deve ser informado!" }';
-		}		
-				
-		if (!cpf) {
-			return '{ "ok" : 0, "msg" : "CPF/CNPJ deve ser informado!" }';
-		}
-		
-		if (!email) {
-			return '{ "ok" : 0, "msg" : "E-mail deve ser informado!" }';
-		}
-		
-		if (!address) {
-			return '{ "ok" : 0, "msg" : "Endereço deve ser informado!" }';
-		}
-		
-		
-		if (!username) {
-			return '{ "ok" : 0, "msg" : "Nome de usuário deve ser informado!!" }';
-		}
-		
-		if (!password) {
-			return '{ "ok" : 0, "msg" : "Senha deve ser informada!" }';			
-		} else if (password != passwordRepeat) {
-			return '{ "ok" : 0, "msg" : "Senhas não coincidem!" }';	
-		}
-
-		if (password.length < 6) {
-			return '{ "ok" : 0, "msg" : "Senha deve conter pelo menos 6 caracteres!" }';
-		} 
-		
-		return;
-	}
-
 	return this;
 }
--- web/public/js/submit.js
+++ web/public/js/submit.js
@@ -1,7 +1,6 @@
 $(function(){				
 	$('#btnSubmit').click(function(e){
-		e.preventDefault();
-						 
+								 
 		var cpf = document.getElementById("cpf").value;
 		var businessName = document.getElementById("businessName").value;
 		var email = document.getElementById("email").value;		
@@ -14,32 +13,73 @@ $(function(){
 		var password = document.getElementById("password").value;
 		var passwordRepeat = document.getElementById("passwordRepeat").value;
 		var personalName = document.getElementById("personalName").value;
-				
-		var data =  { 
-			cpf: cpf, 
-			businessName: businessName,
-			personalName: personalName,
-			email: email,
-			address: address,
-			phone: phone,
-			socialNetwork: socialNetwork,
-			username: username,
-			password: password,
-			passwordRepeat: passwordRepeat};
-			
-				
-		$.ajax({
-			type: 'POST',
-			data: data,			
-			url: '/add-profissional',						
-			success: function(data) {
-				console.log('success');
-				console.log(JSON.stringify(data));				
-				alert(data.msg);								
-			}, 
-			error: function (request, status, error) {
-				alert(request.responseText);
-			}
+		var services = []
+		
+		
+		$('#check-services :checked').each(function() {
+		   services.push($(this).val());
 		});
-	});				
+		      		
+		if (cpf && personalName && businessName &&  email && address && phone && username && password && passwordRepeat) {
+			
+			var error = false;
+			if (password != passwordRepeat) {
+				alert('Senhas não coincidem!');
+					error = true;
+			} else if (password.length < 6) {
+				alert('Senha deve conter pelo menos 6 caracteres!');
+				error = true;
+			} else {
+				error = false;
+			}
+			
+			if (error == false) {				
+				var data =  { 
+					cpf: cpf, 
+					businessName: businessName,
+					personalName: personalName,
+					email: email,
+					address: address,
+					phone: phone,
+					socialNetwork: socialNetwork,
+					services: services,
+					username: username,
+					password: password,
+					passwordRepeat: passwordRepeat};
+				
+				$.ajax({
+					type: 'POST',
+					data: data,	
+					url: '/add-profissional',						
+					success: function(data) {
+						console.log('success');
+						console.log(data);	
+						if (typeof data == "string") {
+							data = jQuery.parseJSON(data); 
+						}								
+						alert(data.msg);								
+					}, 
+					error: function (request, status, error) {
+						alert(request.responseText);
+					}
+				});	
+			}
+			document.getElementById("register-service").focus();
+		}
+		
+		
+	});	
+
+
+	var requiredCheckboxes = $('.options :checkbox[required]');
+	requiredCheckboxes.change(function(){
+		if(requiredCheckboxes.is(':checked')) {
+			requiredCheckboxes.removeAttr('required');
+		} else {
+			requiredCheckboxes.attr('required', 'required');
+		}
+	});
+	
+	
+
 });
