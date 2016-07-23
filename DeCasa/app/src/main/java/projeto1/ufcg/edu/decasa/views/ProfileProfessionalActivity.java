a6aa9d7407139558511f5b68138affa573f3831a
MariaDaniela <maria.henrique@ccc.ufcg.edu.br>
2016-07-23 11:17:44 -0300

    Função DIAL no imagebutton ok

--- DeCasa/app/src/main/java/projeto1/ufcg/edu/decasa/views/ProfileProfessionalActivity.java
+++ DeCasa/app/src/main/java/projeto1/ufcg/edu/decasa/views/ProfileProfessionalActivity.java
@@ -1,15 +1,32 @@
 package projeto1.ufcg.edu.decasa.views;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 import projeto1.ufcg.edu.decasa.R;
 
 public class ProfileProfessionalActivity extends AppCompatActivity {
 
+    private ImageButton ib_call;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile_professional);
+
+        ib_call = (ImageButton) findViewById(R.id.ib_phone_professional);
+
+        ib_call.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                Uri uri = Uri.parse("tel:" + "987098580");//TODO colocar telefone do profissional quando recuperar
+                Intent intent = new Intent(Intent.ACTION_DIAL, uri);
+                startActivity(intent);
+            }
+        });
     }
 }
