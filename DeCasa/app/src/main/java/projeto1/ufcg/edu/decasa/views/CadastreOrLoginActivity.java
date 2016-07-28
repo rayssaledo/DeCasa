e072f3bf7f5faf165b54b9cef151650e5a0a0079
Lucas Marlon Lira Marques <lucas.marques@embedded.ufcg.edu.br>
2016-07-28 15:30:41 -0300

    Tela de splash finalizada e tela de cadastro iniciada.

---
+++ DeCasa/app/src/main/java/projeto1/ufcg/edu/decasa/views/CadastreOrLoginActivity.java
@@ -0,0 +1,46 @@
+package projeto1.ufcg.edu.decasa.views;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+import projeto1.ufcg.edu.decasa.R;
+
+public class CadastreOrLoginActivity extends AppCompatActivity {
+
+    private Button btn_register;
+    private Button btn_login;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cadastre_or_login);
+
+        btn_register = (Button) findViewById(R.id.btn_register);
+        btn_login = (Button) findViewById(R.id.btn_login);
+
+        btn_register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                setView(CadastreOrLoginActivity.this, UserCadastreActivity.class);
+            }
+        });
+
+        btn_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+    }
+
+    public void setView(Context context, Class classe){
+        Intent it = new Intent();
+        it.setClass(context, classe);
+        startActivity(it);
+    }
+
+}
