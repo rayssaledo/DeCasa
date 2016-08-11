e87f723102e02b0cd618c861c1f65ceecff96d7e
MariaDaniela <maria.henrique@ccc.ufcg.edu.br>
2016-08-10 23:39:36 -0300

    Tela de Sobre criada

---
+++ DeCasa/app/src/main/java/projeto1/ufcg/edu/decasa/views/AboutActivity.java
@@ -0,0 +1,37 @@
+package projeto1.ufcg.edu.decasa.views;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.view.View;
+
+import projeto1.ufcg.edu.decasa.R;
+
+public class AboutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                finish();
+                break;
+            default:
+                break;
+        }
+        return true;
+    }
+
+}
