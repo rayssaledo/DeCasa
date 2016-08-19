ab4420a3111e4d880d1c00b11a4ebd8db7fbc96a
marcosasn <marcos.nascimento@ccc.ufcg.edu.br>
2016-08-19 16:17:38 -0300

    Inserindo o título das abas na tela de meus favoritos.

--- DeCasa/app/src/main/java/projeto1/ufcg/edu/decasa/views/MyFavoritesActivity.java
+++ DeCasa/app/src/main/java/projeto1/ufcg/edu/decasa/views/MyFavoritesActivity.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TabHost;
 
 import projeto1.ufcg.edu.decasa.R;
 
@@ -13,6 +14,24 @@ public class MyFavoritesActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_favorites);
+
+        TabHost mTabHost = (TabHost) findViewById(R.id.tabHost);
+        mTabHost.setup();
+
+        TabHost.TabSpec descritor = mTabHost.newTabSpec("aba1");
+        descritor.setContent(R.id.tab_plumber);
+        descritor.setIndicator("Plumber");
+        mTabHost.addTab(descritor);
+
+        descritor = mTabHost.newTabSpec("aba2");
+        descritor.setContent(R.id.tab_electrician);
+        descritor.setIndicator("Electrician");
+        mTabHost.addTab(descritor);
+
+        descritor = mTabHost.newTabSpec("aba3");
+        descritor.setContent(R.id.tab_fitter);
+        descritor.setIndicator("Fitter");
+        mTabHost.addTab(descritor);
     }
 
     public static void setView(Context context, Class classe){
